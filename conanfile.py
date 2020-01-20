588e20a287bd936aba0e4cc68deac1242d695ff2
loic <loic@trilogic.fr>
2020-01-20 16:52:58 +0100

    Getting ready for 2001.0/Windows

--- conanfile.py
+++ conanfile.py
@@ -8,14 +8,14 @@ import os
 def download_x86_64_winsdk (self):
     if 'Release' in self.settings.build_type:
         if 'MD' in self.settings.compiler.runtime:
-            tools.get ('https://files.trilogic.fr/public/483618/dl/vc17_x86_64_md_release.zip')
+            tools.get ('https://files.trilogic.fr/public/cbfbe1/dl/vc16_x86_64_md_release.zip')
         else:
-            tools.get ('https://files.trilogic.fr/public/b6adf7/dl/vc17_x86_64_mt_release.zip')
+            tools.get ('https://files.trilogic.fr/public/204474/dl/vc16_x86_64_mt_release.zip')
     else:
         if 'MD' in self.settings.compiler.runtime:
-            tools.get ('https://files.trilogic.fr/public/bc00b0/dl/vc17_x86_64_md_debug.zip')
+            tools.get ('https://files.trilogic.fr/public/2988e9/dl/vc16_x86_64_md_debug.zip')
         else:
-            tools.get ('https://files.trilogic.fr/public/f1d800/dl/vc17_x86_64_mt_debug.zip')
+            tools.get ('https://files.trilogic.fr/public/4980ce/dl/vc16_x86_64_mt_debug.zip')
 
 #
 # Winsdk 32-bits.
@@ -23,14 +23,14 @@ def download_x86_64_winsdk (self):
 def download_x86_winsdk (self):
     if 'Release' in self.settings.build_type:
         if 'MD' in self.settings.compiler.runtime:
-            tools.get ('https://files.trilogic.fr/public/7b81c0/dl/vc17_x86_md_release.zip')
+            tools.get ('https://files.trilogic.fr/public/62a8ab/dl/vc16_x86_md_release.zip')
         else:
-            tools.get ('https://files.trilogic.fr/public/318f40/dl/vc17_x86_mt_release.zip')
+            tools.get ('https://files.trilogic.fr/public/672c83/dl/vc16_x86_mt_release.zip')
     else:
         if 'MD' in self.settings.compiler.runtime:
-            tools.get ('https://files.trilogic.fr/public/ab7798/dl/vc17_x86_md_debug.zip')
+            tools.get ('https://files.trilogic.fr/public/47dc0c/dl/vc16_x86_md_debug.zip')
         else:
-            tools.get ('https://files.trilogic.fr/public/06a3bd/dl/vc17_x86_mt_debug.zip')
+            tools.get ('https://files.trilogic.fr/public/142d69/dl/vc16_x86_mt_debug.zip')
 
 #
 # Dektec Conan package.
@@ -59,11 +59,11 @@ class DektecDtapiConan (ConanFile):
                 self.run ('ar cru libdtapi.a LinuxSDK/DTAPI/Lib/GCC5.1_CXX11_ABI1/DTAPI.o')
         elif self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
             # Do some check againt static runtime to inform user.
-            if self.settings.compiler.version != 15:
+            if self.settings.compiler.version != 16:
                 if 'MT' in self.settings.compiler.runtime:
-                    raise ConanException('Visual Studio 17 (VC15) is required, yours is VC%s' % self.settings.compiler.version)
+                    raise ConanException('Visual Studio 19 (VC16) is required, yours is VC%s' % self.settings.compiler.version)
                 else:
-                    self.output.warn ('Visual Studio 17 (VC15) redist will be needed !')
+                    self.output.warn ('Visual Studio 19 (VC16) redist will be needed !')
             # Download requested Winsdk.
             if self.settings.arch_build == 'x86_64':
                 download_x86_64_winsdk (self)
